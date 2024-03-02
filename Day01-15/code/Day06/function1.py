b421bff7bbf8fea9eb790fcbb5eab154f8dbac64
djm1990 <djm1990@sina.com>
2024-03-02 15:19:12 +0800

    测试：for in 表示遍历其中的元素

--- Day01-15/code/Day06/function1.py
+++ Day01-15/code/Day06/function1.py
@@ -16,3 +16,19 @@ def factorial(n):
 
 
 print(factorial(7) // factorial(3) // factorial(4))
+
+# 在参数名前面的*表示args是一个可变参数
+def add(*args):
+    total = 0
+    for val in args:
+        total += val
+    return total
+
+
+# 在调用add函数时可以传入0个或多个参数
+print(add())
+print(add(1))
+print(add(1, 2))
+print(add(1, 2, 3))
+print(add(1, 3, 5, 7, 9))
+#测试：for in 表示遍历其中的元素
