89cbbad341d4c4b5fc80a022b89bc6d7a47251ce
djm1990 <djm1990@sina.com>
2024-03-03 20:35:21 +0800

    增加sorted用法

--- Day01-15/code/Day07/string_test.py
+++ Day01-15/code/Day07/string_test.py
@@ -49,3 +49,54 @@ list1 = [1, 3, 5, 7, 100]
 print(list1)
 for index, elem in enumerate(list1):
     print(index, elem)
+
+#对列表进行排序
+a = [5,3,4,2,1]
+print(sorted(a))
+
+#对元组进行排序
+a = (5,4,3,1,2)
+print(a)
+print(sorted(a))
+
+#字典默认按照key进行排序
+a = {4:1,\
+ 5:2,\
+ 3:3,\
+ 2:6,\
+ 1:8}
+print(sorted(a.items()))
+
+#对集合进行排序
+a = {1,5,3,2,4}
+print(sorted(a))
+
+#对字符串进行排序
+a = "51423"
+print(sorted(a))
+
+chars=['http://c.biancheng.net',\
+ 'http://c.biancheng.net/python/',\
+ 'http://c.biancheng.net/shell/',\
+ 'http://c.biancheng.net/java/',\
+ 'http://c.biancheng.net/golang/']
+#默认排序
+print(sorted(chars))
+
+#自定义按照字符串长度排序
+print(sorted(chars,key=lambda x:len(x)))
+
+list1 = ['orange', 'apple', 'zoo', 'internationalization', 'blueberry']
+list2 = sorted(list1)
+# sorted函数返回列表排序后的拷贝不会修改传入的列表
+# 函数的设计就应该像sorted函数一样尽可能不产生副作用
+list3 = sorted(list1, reverse=True)
+# 通过key关键字参数指定根据字符串长度进行排序而不是默认的字母表顺序
+list4 = sorted(list1, key=len)
+print(list1)
+print(list2)
+print(list3)
+print(list4)
+# 给列表对象发出排序消息直接在列表对象上进行排序
+list1.sort(reverse=True)
+print(list1)
