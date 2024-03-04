fc7ee8146bbaffe5c25ac21e435cbce53bd9fc4c
djm1990@sina.com <djm1990@sina.com>
2024-03-04 11:04:05 +0800

    增加字典打印

--- Day01-15/code/Day07/dict1.py
+++ Day01-15/code/Day07/dict1.py
@@ -11,6 +11,7 @@ def main():
     scores = {'骆昊': 95, '白元芳': 78, '狄仁杰': 82}
     print(scores['骆昊'])
     print(scores['狄仁杰'])
+    print(scores)
     for elem in scores:
         print('%s\t--->\t%d' % (elem, scores[elem]))
     scores['白元芳'] = 65
@@ -21,9 +22,11 @@ def main():
         print(scores['武则天'])
     print(scores.get('武则天'))
     print(scores.get('武则天', 60))
+    print(scores)
     print(scores.popitem())
     print(scores.popitem())
     print(scores.pop('骆昊', 100))
+    print(scores)
     scores.clear()
     print(scores)
 
